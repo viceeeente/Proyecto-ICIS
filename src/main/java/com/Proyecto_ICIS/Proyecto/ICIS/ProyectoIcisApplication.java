bfd6e1db10c266d753cce2bd3a2d105d1999ddef
vice. <vice.alvarezm@duocuc.cl>
2025-06-23 18:06:31 -0400

    1.0.0 Primer Commit

---
+++ src/main/java/com/Proyecto_ICIS/Proyecto/ICIS/ProyectoIcisApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto_ICIS.Proyecto.ICIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIcisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIcisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto_ICIS/Proyecto/ICIS/ProyectoIcisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto_ICIS.Proyecto.ICIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIcisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
