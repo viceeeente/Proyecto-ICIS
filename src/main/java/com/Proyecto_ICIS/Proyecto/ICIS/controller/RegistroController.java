289b1676b825df8c62da3efb48e42b37de9061a5
vice. <vice.alvarezm@duocuc.cl>
2025-06-23 19:01:05 -0400

    1.6.4 Modificacion de modelo Usuario, su controller y login.html

--- src/main/java/com/Proyecto_ICIS/Proyecto/ICIS/controller/RegistroController.java
+++ src/main/java/com/Proyecto_ICIS/Proyecto/ICIS/controller/RegistroController.java
@@ -1,10 +1,10 @@
 package com.Proyecto_ICIS.Proyecto.ICIS.controller;
 
-import ch.qos.logback.core.model.Model;
 import com.Proyecto_ICIS.Proyecto.ICIS.model.Usuario;
 import com.Proyecto_ICIS.Proyecto.ICIS.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,15 +15,18 @@ public class RegistroController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @GetMapping("/registro")
-    public String mostrarRegistro(){
-        return "registro";
+    @GetMapping("/register")
+    public String mostrarRegistro(Model model){
+        model.addAttribute("usuario", new Usuario());
+        return "register";
     }
 
-    @PostMapping("/registro")
-    public String procesarRegistro(@ModelAttribute Usuario usuario, Model model) {
+    @PostMapping("/register")
+    public String procesarRegistro(@ModelAttribute Usuario usuario) {
         usuarioService.guardar(usuario);
         return "/login";
     }
+
+
 
 }
--- src/main/java/com/Proyecto_ICIS/Proyecto/ICIS/model/Usuario.java
+++ src/main/java/com/Proyecto_ICIS/Proyecto/ICIS/model/Usuario.java
@@ -20,7 +20,7 @@ public class Usuario {
     private Integer run;
 
     @Column(nullable = false)
-    private Integer dv;
+    private String dv;
 
     @Column(nullable = false)
     private String nombre;
