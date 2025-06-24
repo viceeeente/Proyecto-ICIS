f151626253d7d7bb4db6eafd6959a8f9c73c158c
vice. <vice.alvarezm@duocuc.cl>
2025-06-23 22:58:26 -0400

    1.17.12 Creacion de los css y modificacion de las paginas junto a TramiteController

--- src/main/java/com/Proyecto_ICIS/Proyecto/ICIS/controller/TramiteController.java
+++ src/main/java/com/Proyecto_ICIS/Proyecto/ICIS/controller/TramiteController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.Principal;
+
 @Controller
 public class TramiteController {
 
